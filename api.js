2356613f649ea93b3318a820e77e65ee7590ee69
ddulesov <dmitry.dulesov@gmail.com>
2020-12-10 09:47:33 +0300

    initial commit

---
+++ api.js
@@ -0,0 +1,269 @@
+// Import
+import {
+    ApiPromise,
+    WsProvider
+} from '@polkadot/api';
+import {
+    Keyring
+} from '@polkadot/api';
+import dotenv  from 'dotenv';
+import {
+    readFileSync
+} from 'fs';
+dotenv.config();
+
+
+const DEFAULT_MINCAP_DEADLINE = 3600 * 1000; // msec
+
+const keyring = new Keyring({
+    type: 'sr25519'
+});
+
+class NodeConnection {
+    callback = [];
+	nonce = 1;
+    constructor(api) {
+        this.api = api;
+    }
+
+    async now() {
+        const bn = await this.api.query.timestamp.now();
+        return bn.toNumber();
+    }
+
+    createType() {
+        return this.api.createType(arguments[0], arguments[1]);
+    }
+
+    wait_until(moment) {
+        return new Promise((resolve, reject) => {
+            const m = (moment == 0) ? "unshift" : "push";
+
+            this.callback[m]({
+                moment,
+                resolve,
+                reject
+            });
+        });
+    }
+
+    stop_callback() {
+        this.unsub();
+        for (let item of this.callback) {
+            item.reject("stopping");
+        }
+        this.callback = [];
+        this.api = null;
+    }
+
+    async create_account(addr, role, amount) {
+        await this.api.tx.evercity.accountAddWithRoleAndData(addr, role, 0).signAndSend(this.master, {
+            nonce: -1
+        });
+        await this.api.tx.balances.transfer(addr, amount).signAndSend(this.master, {
+            nonce: -1
+        });		
+    }
+
+    async request_everusd(account, amount) {
+        await this.api.tx.evercity.tokenMintRequestCreateEverusd(amount).signAndSend(account, {
+            nonce: -1
+        });
+    }
+
+    async mint(account, custodian, amount) {
+        await this.api.tx.evercity.tokenMintRequestCreateEverusd(amount).signAndSend(account, {
+            nonce: -1
+        });
+        await this.api.tx.evercity.tokenMintRequestConfirmEverusd(account.address, amount).signAndSend(custodian, {
+            nonce: -1
+        });
+    }
+
+    async account_balance(account) {
+        const balance = await this.api.query.evercity.balanceEverUSD(account);
+        return balance.toNumber();
+    }
+
+    async prepare_bond(issuer, bondid, bond) {
+        const now = await this.api.query.timestamp.now();
+        bond.mincap_deadline = now.toNumber() + DEFAULT_MINCAP_DEADLINE;
+        await this.api.tx.evercity.bondAddNew(bondid, bond).signAndSend(issuer, {
+            nonce: -1
+        });
+    }
+
+    async get_bond(bondid) {
+        const tbondid = this.api.createType('BondId', bondid);
+        return await this.api.query.evercity.bondRegistry(tbondid);
+    }
+
+    async release_bond(master, bondid) {
+        const bond = await this.get_bond(bondid);
+
+        await this.api.tx.evercity.bondRelease(bondid, bond.nonce.toNumber()).signAndSend(this.master, {
+            nonce: -1
+        });
+    }
+
+    async activate_bond(master, auditorid, bondid) {
+        // get nonce value
+        const bond = await this.get_bond(bondid);
+
+        await this.api.tx.evercity.bondSetAuditor(bondid, auditorid).signAndSend(this.master, {
+            nonce: -1
+        });
+
+        await this.api.tx.evercity.bondActivate(bondid, bond.nonce.toNumber() + 1).signAndSend(this.master, {
+            nonce: -1
+        });
+        return bond;
+    }
+
+    async buy_bond_units(investor, bondid, count) {
+        const tbondid = this.api.createType('BondId', bondid);
+        const bond = await this.api.query.evercity.bondRegistry(tbondid);
+        await this.api.tx.evercity.bondUnitPackageBuy(bondid, bond.nonce.toNumber(), count).signAndSend(investor, {
+            nonce: -1
+        });
+        return bond;
+    }
+
+    async give_back_bond_units(investor, bondid, count) {
+        await this.api.tx.evercity.bondUnitPackageReturn(bondid, count).signAndSend(investor, {
+            nonce: -1
+        });
+    }
+
+    async transfer_ownership(investor1, investor2, moment, bondid, count, cost) {
+        const lot = this.api.createType('BondUnitSaleLotStructOf', {
+            deadline: moment,
+            new_bondholder: investor2?.address,
+            bond_units: count,
+            amount: cost
+        });
+
+        await this.api.tx.evercity.bondUnitLotBid(bondid, lot).signAndSend(investor1, {
+            nonce: -1
+        });
+        if (investor2 !== null) {
+            await this.api.tx.evercity.bondUnitLotSettle(bondid, investor1.address, lot).signAndSend(investor2, {
+                nonce: -1
+            });
+        }
+    }
+
+    async deposit(issuer, bondid, amount) {
+        const tbondid = this.api.createType('BondId', bondid);
+        await this.api.tx.evercity.bondDepositEverusd(tbondid, amount).signAndSend(issuer, {
+            nonce: -1
+        });
+    }
+
+    async report_send(issuer, bondid, period, impact_data) {
+        const tbondid = this.api.createType('BondId', bondid);
+        await this.api.tx.evercity.bondImpactReportSend(tbondid, period, impact_data).signAndSend(issuer, {
+            nonce: -1
+        });
+    }
+
+    async report_approve(auditor, bondid, period, impact_data) {
+        const tbondid = this.api.createType('BondId', bondid);
+        await this.api.tx.evercity.bondImpactReportApprove(tbondid, period, impact_data).signAndSend(auditor, {
+            nonce: -1
+        });
+    }
+
+    async withdraw_everusd(account, bondid) {
+        const tbondid = this.api.createType('BondId', bondid);
+        await this.api.tx.evercity.bondWithdrawEverusd(tbondid).signAndSend(account, {
+            nonce: -1
+        });
+    }
+
+    async bond_redeem(issuer, bondid) {
+        const tbondid = this.api.createType('BondId', bondid);
+        await this.api.tx.evercity.bondRedeem(tbondid).signAndSend(issuer, {
+            nonce: -1
+        });
+    }
+}
+
+
+async function connect(ws_url) {
+    const ws = process.env.ws_url || "ws://localhost:9944";
+    const wsProvider = new WsProvider(ws);
+
+    const api = await ApiPromise.create({
+        provider: wsProvider,
+        types: JSON.parse(readFileSync(process.env.types || "./types.json", 'utf8'))
+    });
+
+    let o = new NodeConnection(api);
+
+    const unsub = await api.query.timestamp.now((now) => {
+        const moment = now.toNumber();
+        //console.log(`The last block has a timestamp of ${moment}`);
+        let idx = o.callback.findIndex((el, idx, arr) => (el.moment > moment));
+
+        let alarm_callback = [];
+        if (idx == -1) {
+            alarm_callback = o.callback;
+            o.callback = [];
+        } else {
+            alarm_callback = o.callback.splice(0, idx);
+        }
+
+        for (let item of alarm_callback) {
+            item.resolve(moment);
+        }
+    });
+
+    o.unsub = unsub;
+    o.day_duration = api.consts.evercity.dayDuration.toNumber();
+    o.master = keyring.addFromUri('//Alice', {
+        name: 'Alice (master)'
+    });
+
+    // Retrieve the chain name
+    const chain = await api.rpc.system.chain();
+
+    // Retrieve the latest header
+    const lastHeader = await api.rpc.chain.getHeader();
+    // Log the information
+    //console.log(`${chain}: last block #${lastHeader.number} has hash ${lastHeader.hash}`);
+    o.lastHeader = {
+        number: lastHeader.number,
+        hash: lastHeader.hash
+    };
+
+    return o;
+}
+
+
+/*
+await api.rpc.chain.subscribeNewHeads((lastHeader) => {
+  console.log(`${chain}: last block #${lastHeader.number} has hash ${lastHeader.hash}  was authored by ${lastHeader.author}`);
+});
+api.query.system.events((events) => {
+	console.log(`\nReceived ${events.length} events:`);
+
+	// Loop through the Vec<EventRecord>
+	events.forEach((record) => {
+	  // Extract the phase, event and the event types
+	  const { event, phase } = record;
+	  const types = event.typeDef;
+
+	  // Show what we are busy with
+	  console.log(`\t${event.section}:${event.method}:: (phase=${phase.toString()})`);
+	  console.log(`\t\t${event.meta.documentation.toString()}`);
+
+	  // Loop through each of the parameters, displaying the type and data
+	  event.data.forEach((data, index) => {
+		console.log(`\t\t\t${types[index].type}: ${data.toString()}`);
+	  });
+	});
+});
+*/
+
+export default connect
---
+++ index.js
@@ -0,0 +1,281 @@
+import connect from './api.js';
+import {
+    Keyring
+} from '@polkadot/api';
+const keyring = new Keyring({
+    type: 'sr25519'
+});
+let api = await connect();
+
+const BLOCK_INTERVAL = 20000;
+// everusd to account balance translation 
+const UNIT = 1000000000;
+// scenario1 bonds
+const BOND1 = "ECBD101";
+const BOND2 = "ECBD102";
+const BOND3 = "ECBD103";
+const BOND4 = "ECBD104";
+const BOND5 = "ECBD105";
+// scenario2 bond
+const BOND10 = "ECBDSC2";
+
+// account roles
+const CUSTODIAN_ROLE = 2;
+const ISSUER_ROLE = 4;
+const INVESTOR_ROLE = 8;
+const AUDITOR_ROLE = 16;
+const MANAGER_ROLE = 32;
+
+const custodian = keyring.addFromUri('//Custodian0', {
+    name: 'custodian'
+});
+const auditor = keyring.addFromUri('//Auditor0', {
+    name: 'auditor'
+});
+const manager = keyring.addFromUri('//Manager0', {
+    name: 'manager'
+});
+const issuer = keyring.addFromUri('//Issuer0', {
+    name: 'issuer'
+});
+const investor1 = keyring.addFromUri('//Investor01', {
+    name: 'investor_1'
+});
+const investor2 = keyring.addFromUri('//Investor02', {
+    name: 'investor_2'
+});
+const investor3 = keyring.addFromUri('//Investor03', {
+    name: 'investor_3'
+});
+const superissuer = keyring.addFromUri('//SuperIssuer0', {
+    name: 'issuer+investor'
+});
+
+const accounts = [
+    custodian,
+    auditor,
+    manager,
+    issuer,
+    investor1,
+    investor2,
+    investor3,
+    superissuer
+];
+
+for (let account of accounts) {
+    console.log(`account ${account.meta.name} has address ${account.address}`);
+}
+
+function get_bond(day, price) {
+    return {
+        "docs_pack_root_hash_main": [0],
+        "docs_pack_root_hash_legal": [0],
+        "docs_pack_root_hash_finance": [0],
+        "docs_pack_root_hash_tech": [0],
+
+        "impact_data_type": "POWER_GENERATED",
+        "impact_data_baseline": 10000,
+        "impact_data_max_deviation_cap": 16000,
+        "impact_data_max_deviation_floor": 4000,
+        "impact_data_send_period": day,
+        "interest_rate_penalty_for_missed_report": 2000,
+        "interest_rate_base_value": 3000,
+        "interest_rate_margin_cap": 5000,
+        "interest_rate_margin_floor": 1000,
+        "interest_rate_start_period_value": 1000,
+        "interest_pay_period": day,
+        "start_period": 2 * day,
+        "payment_period": 2 * day,
+        "bond_duration": 12,
+        "bond_finishing_period": 100 * day,
+        "mincap_deadline": 0,
+        "bond_units_mincap_amount": 100,
+        "bond_units_maxcap_amount": 600,
+        "bond_units_base_price": price * UNIT
+    }
+}
+
+async function scenario1() {
+    let now = await api.now();
+    await api.wait_until(0);
+	const everusd = 1000;
+    await api.mint(investor1, custodian, everusd * UNIT);
+    await api.mint(investor2, custodian, everusd * UNIT);
+    await api.mint(investor3, custodian, everusd * UNIT);
+
+    console.log(`each investor has minted ${everusd} everusd`);
+
+    const bond1 = get_bond(api.day_duration, 10);
+    await api.wait_until(0);
+
+    await api.prepare_bond(issuer, BOND1, bond1);
+    await api.prepare_bond(issuer, BOND2, bond1);
+    await api.prepare_bond(issuer, BOND3, bond1);
+    await api.prepare_bond(issuer, BOND4, bond1);
+    await api.prepare_bond(issuer, BOND5, bond1);
+
+    console.log(`'${BOND1}' '${BOND2}' '${BOND3}' '${BOND4}' '${BOND5}' prepared`);
+    // RELEASE BONDS
+    now = await api.now();
+    await api.wait_until(0);
+
+    await api.release_bond(api.master, BOND1);
+    await api.release_bond(api.master, BOND2);
+    await api.release_bond(api.master, BOND3);
+    await api.release_bond(api.master, BOND4);
+	console.log(`'${BOND1}' '${BOND2}' '${BOND3}' '${BOND4}' bonds released`);
+	
+    await api.wait_until(0);
+    await api.buy_bond_units(investor1, BOND1, 100);
+    await api.buy_bond_units(investor2, BOND2, 100);
+
+    await api.buy_bond_units(investor1, BOND3, 50);
+    await api.buy_bond_units(investor2, BOND3, 50);
+    
+	console.log(`${investor1.meta.name} bought 150 bond units`);
+    console.log(`${investor2.meta.name} bought 150 bond units`);
+	
+    await api.buy_bond_units(investor3, BOND4, 100);
+    console.log(`${investor3.meta.name} bought 100 bond units`);
+
+    // GIVE BACK SOME BOND UNITS
+    await api.wait_until(0);
+    await api.give_back_bond_units(investor3, BOND4, 100);
+	console.log(`${investor3.meta.name} give back 100 bond units`);
+
+    // ACTIVATE BONDS
+    await api.wait_until(0);
+    await api.activate_bond(api.master, auditor.address, BOND1);
+    await api.activate_bond(api.master, auditor.address, BOND2);
+    await api.activate_bond(api.master, auditor.address, BOND3);
+
+    console.log(`'${BOND1}' '${BOND2}' '${BOND3}' bonds activated`);
+
+    // BUY BOND AFTER ACTIVATION
+    await api.wait_until(0);
+    await api.buy_bond_units(investor1, BOND3, 50);
+
+    console.log(`${investor1.meta.name} augmented bond package by 50 bond units`);
+    // SALE BOND AFTER ACTIVATION
+    await api.wait_until(0);
+    await api.transfer_ownership(investor1, investor3, now + 100 * BLOCK_INTERVAL, BOND3, 50, 600 * UNIT);
+	console.log(`${investor1.meta.name} sold to ${investor3.meta.name} 600 bond units`);
+
+    await api.wait_until(0);
+    await api.transfer_ownership(investor2, null, now + 100 * BLOCK_INTERVAL, BOND3, 50, 600 * UNIT);
+	console.log(`${investor2.meta.name} bid 600 bond units`);
+}
+
+
+async function scenario2() {
+    let now = await api.now();
+	const everusd = 1000;
+	// MINT EVERUSD
+    await api.mint(investor1, custodian, everusd * UNIT);
+    await api.mint(investor2, custodian, everusd * UNIT);
+    await api.mint(investor3, custodian, everusd * UNIT);
+	console.log(`each investor has minted ${everusd} everusd`);
+	
+    const initialBalance = await api.account_balance(issuer.address);
+    console.log(`initial ${issuer.meta.name} balance is ${initialBalance}`);
+
+	// RELEAS BOND
+    const bond = get_bond(api.day_duration, 10);
+
+    await api.prepare_bond(issuer, BOND10, bond);
+    console.log(`'${BOND10}' has been prepared `);
+    await api.wait_until(0);
+
+    await api.release_bond(api.master, BOND10);
+    console.log(`'${BOND10}' has been released `);
+    await api.wait_until(0);
+
+    await api.buy_bond_units(investor1, BOND10, 10);
+    await api.buy_bond_units(investor2, BOND10, 50);
+    await api.buy_bond_units(investor3, BOND10, 100);
+    console.log(`'${BOND10}' bond units bought by`);
+	console.log(`  ${investor1.meta.name} - 10 units`);
+	console.log(`  ${investor2.meta.name} - 50 units`);
+	console.log(`  ${investor3.meta.name} - 100 units`);
+	
+    await api.wait_until(0);
+    await api.activate_bond(api.master, auditor.address, BOND10);
+    console.log(`'${BOND10}' has been activated `);
+    await api.wait_until(0);
+
+    let bond_in_chain = await api.get_bond(BOND10);
+
+    const bond_activation_time = bond_in_chain.active_start_date.toNumber();
+    console.log(`bond activated at ${bond_activation_time}`);
+
+    await api.wait_until(0);
+    const newBalance = await api.account_balance(issuer.address);
+    console.log(`new ${issuer.meta.name} balance is ${newBalance}`);
+    // first report period
+    await api.wait_until(bond_activation_time + 1000 * api.day_duration * 1);
+
+    for (let i = 0; i < 14; i++) {
+        // deposit everusd to bond fund for pay off coupon yield
+        await api.deposit(issuer, BOND10, 5000000);
+        // report period
+
+        await api.report_send(issuer, BOND10, i, 4000 + i * 1000);
+        await api.wait_until(0);
+        await api.report_approve(auditor, BOND10, i, 4000 + i * 1000);
+
+        // after coupon payment period
+        console.log(`wait for ${i} period (${2 * api.day_duration} sec)`);
+        await api.wait_until(bond_activation_time + api.day_duration * 1000 * (2 * i + 3));
+        // first investor withdraw every period
+        await api.withdraw_everusd(investor1, BOND10);
+    }
+
+    await api.mint(issuer, custodian, 4 * UNIT);
+    console.log(`${issuer.meta.name} get extra 4 everusd for pay off principal value`);
+    await api.wait_until(bond_activation_time + api.day_duration * 15);
+    await api.bond_redeem(issuer, BOND10);
+    console.log(`redeem bond '${BOND10}'`);
+    await api.wait_until(0);
+	
+    await api.withdraw_everusd(investor1, BOND10);
+    await api.withdraw_everusd(investor2, BOND10);
+    await api.withdraw_everusd(investor3, BOND10);
+
+    console.log(`withdraw bond '${BOND10}' by all investors`);
+    await api.wait_until(0);
+}
+
+async function main() {
+	console.log("create accounts");
+	
+    const basetokens = 6000000000;	
+	
+	await api.create_account(custodian.address, CUSTODIAN_ROLE, basetokens);
+	await api.create_account(auditor.address, AUDITOR_ROLE, basetokens);
+	await api.create_account(manager.address, MANAGER_ROLE, basetokens);
+	await api.create_account(issuer.address, ISSUER_ROLE, basetokens);
+	await api.create_account(investor1.address, INVESTOR_ROLE, basetokens);
+	await api.create_account(investor2.address, INVESTOR_ROLE, basetokens);
+	await api.create_account(investor3.address, INVESTOR_ROLE, basetokens);
+	await api.create_account(superissuer.address, INVESTOR_ROLE + ISSUER_ROLE, basetokens);
+	
+	
+	console.log("account created");
+	var args = process.argv.slice(2);
+
+	switch (args[0]) {
+		case 'scenario1':
+			await scenario1.apply(api, args.slice(1));
+			break;
+		case 'scenario2':
+			await scenario2.apply(api, args.slice(1));
+			break;
+		default:
+			console.log('account created');
+			break
+	}
+
+	api.stop_callback()
+	console.log("end of script");
+}
+main().catch(console.error).finally(() => process.exit());
